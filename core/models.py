e2c9ed2ae34196fb8ead5203dd9f73b1adeb4266
arpitpardesi <arpit.pardesi6@gmail.com>
2024-06-21 02:09:44 +0530

    basic codes

--- core/models.py
+++ core/models.py
@@ -5,3 +5,6 @@ class Member(models.Model):
   firstname = models.CharField(max_length=255)
   lastname = models.CharField(max_length=255)
   phone = models.IntegerField(null=True)
+
+  def __str__(self):
+    return f"{self.firstname} {self.lastname}"
