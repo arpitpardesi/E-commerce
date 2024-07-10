87874ecd712ecb115ce758cce5ea0677e64bbdba
arpitpardesi <arpit.pardesi6@gmail.com>
2024-07-11 01:12:12 +0530

    implemented 404 and redirect

--- myApp/urls.py
+++ myApp/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index, name='index'),
-    path('test',views.test , name='test')
+    path('', views.index, name='index'),
+    path('test', views.test, name='test'),
+    path('<int:num_page>', views.num_view),
+    path('<str:topic>/', views.new_view),
 
 ]
--- myApp/views.py
+++ myApp/views.py
@@ -1,8 +1,37 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+
+
 # Create your views here.
 def index(request):
     return HttpResponse("<h1>Hello from My App</h1>")
 
+
 def test(request):
     return HttpResponse("<h1>Test function under My App</h1>")
+
+
+articles = {
+    'tech': "<h1>Tech page</h>",
+    'world': "<h1>World page</h>",
+    'cricket': "<h1>Cricket page</h>"
+}
+
+
+def new_view(request, topic):
+    try:
+        r = articles[topic]
+        return HttpResponse(articles[topic])
+    except:
+        raise Http404("404 page not found")
+
+
+def num_view(request, num_page):
+    try:
+        topic_list = list(articles.keys())
+        topic = topic_list[num_page]
+
+        return HttpResponseRedirect(topic)
+
+    except:
+        raise Http404("404 page not found")
