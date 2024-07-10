e7d175649253b2a70524ff917854240ba32135c9
arpitpardesi <arpit.pardesi6@gmail.com>
2024-07-10 23:22:39 +0530

    created myApp views and configured urls.py

--- myApp/views.py
+++ myApp/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello ")
+    return HttpResponse("<h1>Hello from My App</h1>")
+
+def test(request):
+    return HttpResponse("<h1>Test function under My App</h1>")
