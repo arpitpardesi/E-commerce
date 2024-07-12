d529686ca113998b065fded684ca759d3e12e12f
arpitpardesi <arpit.pardesi6@gmail.com>
2024-07-13 00:12:39 +0530

    implementet views and templates basic method

--- myApp/urls.py
+++ myApp/urls.py
@@ -2,9 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('test/', views.test, name='test'),
-    path('<int:num_page>/', views.numPage_view),
-    path('<str:topic>/', views.new_view, name='topic-page'),
-
+    path("", views.simpleView)
 ]
+
+# urlpatterns = [
+#     path('', views.index, name='index'),
+#     path('test/', views.test, name='test'),
+#     path('<int:num_page>/', views.numPage_view),
+#     path('<str:topic>/', views.new_view, name='topic-page'),
+#
+# ]
--- myApp/views.py
+++ myApp/views.py
@@ -4,35 +4,39 @@ from django.urls import reverse
 
 
 # Create your views here.
-def index(request):
-    return HttpResponse("<h1>Hello from My App</h1>")
 
+def simpleView(request):
 
-def test(request):
-    return HttpResponse("<h1>Test function under My App</h1>")
+    return render(request, 'myApp/test.html')
 
-
-articles = {
-    'tech': '<h1>Tech page</h>',
-    'world': '<h1>World page</h>',
-    'cricket': '<h1>Cricket page</h>'
-}
-
-
-def new_view(request, topic):
-    try:
-        # r = articles[topic]
-        return HttpResponse(articles[topic])
-    except:
-        raise Http404("404 page not found")
-
-
-def numPage_view(request, num_page):
-    try:
-        topic_list = list(articles.keys())
-        topic = topic_list[num_page]
-        return HttpResponseRedirect(reverse('topic-page', args=[topic]))
-
-
-    except:
-        raise Http404("404 page not found")
+# def index(request):
+#     return HttpResponse("<h1>Hello from My App</h1>")
+#
+#
+# def test(request):
+#     return HttpResponse("<h1>Test function under My App</h1>")
+#
+#
+# articles = {
+#     'tech': '<h1>Tech page</h>',
+#     'world': '<h1>World page</h>',
+#     'cricket': '<h1>Cricket page</h>'
+# }
+#
+#
+# def new_view(request, topic):
+#     try:
+#         # r = articles[topic]
+#         return HttpResponse(articles[topic])
+#     except:
+#         raise Http404("404 page not found")
+#
+#
+# def numPage_view(request, num_page):
+#     try:
+#         topic_list = list(articles.keys())
+#         topic = topic_list[num_page]
+#         return HttpResponseRedirect(reverse('topic-page', args=[topic])
+#
+#     except:
+#         raise Http404("404 page not found")
