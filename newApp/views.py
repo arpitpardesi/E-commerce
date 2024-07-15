04a1e81e5c0d3a5b2eaebbca45d7ca35df2f8316
arpitpardesi <arpit.pardesi6@gmail.com>
2024-07-16 01:43:38 +0530

    variables in templates

--- newApp/views.py
+++ newApp/views.py
@@ -6,5 +6,5 @@ def exampleView(request):
     return render(request, 'newApp/example.html')
 
 def variableView(request):
-    dict = {"firstName":"Arpit", "lastName": "Pardesi"}
+    dict = {"firstName":"Arpit", "lastName": "Pardesi", 'aList':[1,2,3],'aNewDict':{'newKey':'value'} }
     return render(request, 'newApp/variable.html', context=dict)
