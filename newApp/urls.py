3f19fd5dd27a6ab16ee92abe83d0419802c37757
arpitpardesi <arpit.pardesi6@gmail.com>
2024-07-18 00:46:18 +0530

    url names

--- newApp/urls.py
+++ newApp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 from . import views
 
+app_name = 'newApp'
+
 urlpatterns = [
-    path("", views.exampleView),
-path("variable", views.variableView)
+    path("", views.exampleView, name='example'),
+    path("variable", views.variableView, name='variable')
 ]
