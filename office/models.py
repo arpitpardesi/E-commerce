5b7761733ca80dfc0e580fad9c59d6109fd7c100
arpitpardesi <arpit.pardesi6@gmail.com>
2024-07-20 01:37:08 +0530

    working with SQLite and model.py

--- office/models.py
+++ office/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Patient(models.Model):
+    fName = models.CharField(max_length=50)
+    lName = models.CharField(max_length=50)
+    age = models.IntegerField()
